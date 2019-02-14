07312a79cd8851e66804e1f1d4fa918692a185b5
RmRubel <rm77533@gmail.com>
2019-02-14 21:27:27 +0600

    Change my string

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def verify():
         if not request.args.get("hub.verify_token") == "hello":
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-    return "Hello world", 200
+    return "Hello world Rubel", 200
 
 
 @app.route('/', methods=['POST'])
